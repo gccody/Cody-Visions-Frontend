3c7c2549ba555410684693999620d59ff6150c12
Grant <gccody2010@live.com>
2023-05-17 19:45:52 -0400

    Hero image is started

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,13 @@
+import { Link } from "react-router-dom"
+
 export const Home = () => {
   return (
-    <div>Hellow World!</div>
+    <div>
+      <div className="hero-content">
+        <p className="hero-title" style={{ fontWeight: 'bolder' }}>Cody Visions</p>
+        <p className="hero-description">Available within Hendersonville NC</p>
+      </div>
+      <p style={{ paddingBottom: '100px' }}>Hello</p>
+    </div>
   );
 } 
